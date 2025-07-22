fe587893f85bec86df2557053a85dc4cb2a033f0
minjuice98 <1105kmj@naver.com>
2025-07-22 17:18:22 +0900

    feat: initial_map

---
+++ MapTool/img/overworld-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="overworld-sheet" tilewidth="8" tileheight="8" tilecount="3825" columns="85">
+ <image source="overworld-sheet.png" width="684" height="365"/>
+</tileset>
